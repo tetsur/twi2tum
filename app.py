f2a8d13f7cdb3114cc18f52eaef96e8a48da1c08
odmishien <tmtm.yagi.007@gmail.com>
2020-08-05 15:36:31 +0900

    tweetが途中で切れてまう問題

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ tum_api = Tumblpy(tum_consumer_key, tum_consumer_secret, tum_oauth_token, tum_oa
 
 def get_latest_fav(tweet_id):
     fav = {}
-    latest_fav_tweet = twi_api.get_status(tweet_id)
+    latest_fav_tweet = twi_api.get_status(tweet_id, tweet_mode='extended')
     latest_fav_tweet_text = re.sub(r'https://t.co/\w*','',latest_fav_tweet.text)
     fav["text"] = latest_fav_tweet_text
     if hasattr(latest_fav_tweet, "extended_entities"):
